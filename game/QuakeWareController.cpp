2d4dc57b19bae784a0f0e7eef51111a3da3e11cf
adamr5000 <adamr5000@hotmail.com>
2016-02-28 18:04:09 -0500

    Creation of QuakeWareController and StatusEffect classes. Currently untested!

---
+++ game/QuakeWareController.cpp
@@ -0,0 +1,13 @@
+/*
+  QuakeWareController.cpp (for QuakeWare mod) by amr48
+  Manages instantiation of microgames, application/removal of status effects, and corresponding HUD elements.
+*/
+
+#include "../idlib/precompiled.h"
+#pragma hdrstop
+
+#include "Game_local.h"
+
+void idStatusEffect::Spawn() {
+
+}
